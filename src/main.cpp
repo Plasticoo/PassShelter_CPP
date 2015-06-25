8fe6202a435a2cea3acf7f38af695738c1a18e0e
Romeu Gomes <oh4ianon@hotmail.com>
2015-06-25 17:20:10 +0000

    Splits user string.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <sstream>
+
+#include <vector>
 
 int main(int argc, char* argv[])
 {
 	std::string user_cmd;
 
 	std::cout << "Insert command: \n";
-	std::cin >> user_cmd;
+	std::getline(std::cin, user_cmd);
 
+	std::istringstream cmd_buffer(user_cmd);
+
+	while(cmd_buffer)
+	{
+		std::string temp;
+		cmd_buffer >> temp;
+		std::cout << temp << "\n";
+	}
+
+	return 0;
 }
