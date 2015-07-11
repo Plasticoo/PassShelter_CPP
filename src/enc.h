16358e2ddf8e3272de15ac4d158e13069e29d473
Romeu Gomes <oh4ianon@hotmail.com>
2015-07-11 02:07:31 +0000

    Genkey command for debugging purposes.

--- src/enc.h
+++ src/enc.h
@@ -3,10 +3,9 @@
 
 #include <stdint.h>
 
-#define IV_SIZE 16
-
-void generate_iv(uint8_t output_buf[IV_SIZE]);
+void generate_iv(uint8_t output_buf[]);
+void generate_key(uint8_t output_buf[]);
+void generate_string(uint8_t output_buf[], int key_size);
 void print_encripted_key(uint8_t buffer[], int b_size);
-void aes_encrypt_cbc();
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -55,6 +55,8 @@ int main(int argc, char* argv[])
 	std::string user_cmd;
 	std::vector<std::string> cmd_words;
 
+	bool loop = true;
+
 	print_stats();
 
 	if(!file_exists(db_file))
@@ -67,44 +69,62 @@ int main(int argc, char* argv[])
 		std::cout << "File 'pshelter.db' found.\n";
 	}
 
-	std::cout << "Insert command: \n";
-	std::getline(std::cin, user_cmd);
-
-	cmd_words = split_string(user_cmd);
-
-	if(cmd_words.size() != 0)
+	while(loop)
 	{
-		if (cmd_words[0] == "ins")
+		std::cout << "Insert command: \n";
+		std::getline(std::cin, user_cmd);
+
+		cmd_words = split_string(user_cmd);
+
+		if(cmd_words.size() != 0)
 		{
-			if(cmd_words.size() == 4)
+			if (cmd_words[0] == "ins")
 			{
-				sql_class.sql_insert(cmd_words[1], cmd_words[2], cmd_words[3]);	
+				if(cmd_words.size() == 4)
+				{
+					sql_class.sql_insert(cmd_words[1], cmd_words[2], cmd_words[3]);	
+				}
+				else
+				{
+					std::cout << "Wrong number of arguments.\n";
+				}
 			}
+			else if(cmd_words[0] == "rm")
+			{
+				if(cmd_words.size() == 4)
+				{
+					sql_class.sql_delete(cmd_words[1], cmd_words[2], cmd_words[3]);	
+				}
+				else
+				{
+					std::cout << "Wrong number of arguments.\n";
+				}
+			}
+			else if(cmd_words[0] == "genkey")
+			{
+				uint8_t buff[32];
+				uint8_t iv[16];
+				uint8_t key[16];
+				uint8_t word[32];
+
+				generate_iv(iv);
+				generate_key(key);
+				generate_string(word, 32);
+				AES128_CBC_encrypt_buffer(buff, word, 32, key, iv);
+
+				print_encripted_key(buff, 32);
+			}
+			else if(cmd_words[0] == "exit" || cmd_words[0] == "quit")
+			{
+				// loop = false;
+				exit(EXIT_SUCCESS);	
+			}
+
 			else
 			{
-				std::cout << "Wrong number of arguments.\n";
+				std::cout << "Invalid command.\n";
 			}
-		}
-		else if(cmd_words[0] == "rm")
-		{
-			if(cmd_words.size() == 4)
-			{
-				sql_class.sql_delete(cmd_words[1], cmd_words[2], cmd_words[3]);	
-			}
-			else
-			{
-				std::cout << "Wrong number of arguments.\n";
-			}
-		}
-		else if(cmd_words[0] == "exit" || cmd_words[0] == "quit")
-		{
-			exit(EXIT_SUCCESS);	
-		}
-		else
-		{
-			std::cout << "Invalid command.\n";
 		}
 	}
-
 	return 0;
 }
