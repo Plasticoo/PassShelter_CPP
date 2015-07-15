ec2baeb226e235a43ba2b04c78133047f8762089
Romeu Gomes <oh4ianon@hotmail.com>
2015-07-15 15:58:34 +0000

    Changed callback function parameter name

--- src/sqlite.cpp
+++ src/sqlite.cpp
@@ -28,14 +28,14 @@ SQLite::~SQLite()
 }
 
 // function that prints table info
-int SQLite::callback(void* not_used, int argc, char** argv, char** azCol_name)
+int SQLite::callback(void* not_used, int argc, char** argv, char** col_name)
 {
 	int i;
 	not_used = 0;
 
 	for (i = 0; i < argc; i++) 
 	{
-		printf("%s = %s\n", azCol_name[i], argv[i] ? argv[i] : "NULL");	
+		printf("%s: %s | ", col_name[i], argv[i] ? argv[i] : "NULL");	
 	}
 
 	putchar('\n');
